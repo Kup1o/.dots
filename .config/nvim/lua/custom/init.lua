818012cf52cb742708d0db6354e371afa71eed08
kup1o <kup1o@pm.me>
2023-05-09 07:36:51 +0300

    feat(nvim): add tabufline buffer numbers, switch hotkeys

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,18 +1,17 @@
--- local autocmd = vim.api.nvim_create_autocmd
-
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+local autocmd = vim.api.nvim_create_autocmd
 
 -- enable rc filetype highlighting with nvim-treesitter 
 -- set rust ft to .rc
-local autocmd = vim.api.nvim_create_autocmd
-
 autocmd("FileType", {
   pattern = "rc",
   callback = function()
     vim.bo.ft = "rust"
   end,
 })
+
+-- enable numbers of buffers in tabufline
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
