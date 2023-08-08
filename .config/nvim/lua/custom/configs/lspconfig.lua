acee46afeec21c7f744736b9d5995b9d9710902d
kup1o <kup1o@pm.me>
2023-08-08 07:51:37 +0300

    fix(nvim): emmet support for additional filetypes

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -1,3 +1,12 @@
 local lspconfig = require "lspconfig"
 
-lspconfig.emmet_ls.setup {}
+lspconfig.emmet_ls.setup {
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    init_options = {
+      html = {
+        options = {
+          ["bem.enabled"] = true,
+        },
+      },
+    }
+}
