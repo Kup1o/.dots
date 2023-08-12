a98f739d12603e52b9fc37bde64b503c9ab8cb54
kup1o <kup1o@pm.me>
2023-08-12 18:58:43 +0300

    feat(nvim): theprimeagen's nvim remaps
    https://www.youtube.com/watch?v=w7i4amO_zaE

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -7,10 +7,23 @@ M.general = {
     ["<leader>ha"] = { "<cmd> :HopAnywhere <cr>", "go to anywhere" },
     ["<leader>hc"] = { "<cmd> :HopChar1 <cr>", "go to any character" },
     ["<leader>hw"] = { "<cmd> :HopWord <cr>", "go to any word" },
+    ["<leader>qs"] = { "<cmd> :leaderleaderleaderleader /\\w\\+/j % | copen <cr>", "open better quickfix" },
+    ["<leader>s"] = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]] },
+    ["<leader>sh"] = { "<cmd> :Gitsigns stage_hunk <cr>", "stage hunk" },
     ["<leader>tb"] = { "<cmd> :TagbarToggle <cr>", "open tagbar" },
     ["<leader>tt"] = { "<cmd> :TodoTelescope <cr>", "search todos" },
-    ["<leader>sh"] = { "<cmd> :Gitsigns stage_hunk <cr>", "stage hunk" },
     ["<leader>zm"] = { "<cmd> :ZenMode <cr>", "activate zenmode" },
+    ["<leader>["] = { ":!chmod +x %<CR>", "make an opened file executable" },
+    ["<C-d>"] = { "<C-d>zz", "scroll down, cursor stays in the middle" },
+    ["<C-u>"] = { "<C-u>zz", "scroll down, cursor stays in the middle" },
+    ["J"] = { "mzJ`z", "take a line below to the current, cursor stays" },
+    ["n"] = { "nzzzv", "search next, cursor stays in the middle" },
+    ["N"] = { "Nzzzv", "search previous, cursor stays in the middle" },
+  },
+
+  v = {
+    ["J"] = { ":m '>+1<CR>gv=gv", "Move selected lines down" },
+    ["K"] = { ":m '<-2<CR>gv=gv", "Move selected lines up" },
   },
 }
 
